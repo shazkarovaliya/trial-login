e7da523b3bcdb71fb64fbf6f0b586425850352ff
Shaz Karovaliya <shazkarovaliya@gmail.com>
2025-02-03 19:54:00 -0600

    login changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3001/login' || 'https://vamsivemula.art/login', {
+      const response = await fetch(/* 'http://localhost:3001/login' */ 'https://vamsivemula.art/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
