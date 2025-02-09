909da518ccafe5068cce1edd3f282620656fab04
Shaz Karovaliya <shazkarovaliya@gmail.com>
2025-02-09 17:50:22 -0600

    checkers added

--- frontend/src/components/UserContext.js
+++ frontend/src/components/UserContext.js
@@ -14,8 +14,10 @@ export const UserProvider = ({ children }) => {
         });
         if (response.ok) {
           const data = await response.json();
+          console.log('Logged In');
           setIsLoggedIn(data.isLoggedIn); // true if session exists
         } else {
+          console.log('Logged Out');
           setIsLoggedIn(false);
         }
       } catch (error) {
