5eac7c3ba894719bb2aeb218a01c98eee98dc0d1
Shaz Karovaliya <shazkarovaliya@gmail.com>
2025-02-19 17:54:41 -0600

    main.css

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import '../App.css';
+import '../css/Main.css';
 
 const Main = () => {
   const navigate = useNavigate();
